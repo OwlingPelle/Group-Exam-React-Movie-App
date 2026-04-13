6f2fcd4cb21e4ab8a9232b6f2960136e2e3813e3
OwlingPelle <per.gronroos@hotmail.com>
2026-04-13 14:14:50 +0200

    header klar

---
+++ src/components/Favorites/Favorites.jsx
@@ -0,0 +1,20 @@
+import './favorites.css';
+import { Link } from 'react-router-dom';
+
+const Favorites = () => {
+	return (
+		<section className='header__favorites'>
+			<div className='header__fav-btn' id='favBtn'>
+				<i className='fa-solid fa-star header__fav-star'></i>
+				<Link to='/favorites' className='header__fav-text'>
+					Favorites
+				</Link>
+			</div>
+			<div className='header__count-icon d-none' id='favCountContainer'>
+				<p className='header__count-number' id='favCount'></p>
+			</div>
+		</section>
+	);
+};
+
+export default Favorites;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,28 +1,17 @@
-import React from 'react';
+import SearchField from '../SearchField/SearchField';
+import Favorites from '../Favorites/Favorites.jsx';
+import './header.css';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
 	return (
-		<header class='header'>
-			<div class='content-wrapper header__flex'>
-				<a href='index.html' class='fa-solid fa-house header__home-icon'></a>
-				<a href='index.html'>
-					<img class='header__logo' src='./res/logo.svg' alt='logotype' />
-				</a>
-				<form class='header__form' id='searchForm'>
-					<input class='header__input' id='searchInput' type='text' aria-label='Search' />
-					<button class='header__form-btn' id='searchBtn' aria-label='Search-button'>
-						<i class='fa-solid fa-magnifying-glass'></i>
-					</button>
-				</form>
-				<section class='header__favorites'>
-					<a href='./favorites.html' class='header__fav-btn' id='favBtn'>
-						<i class='fa-solid fa-star header__fav-star'></i>
-						<span class='header__fav-text'>Favorites</span>
-					</a>
-					<div class='header__count-icon d-none' id='favCountContainer'>
-						<p class='header__count-number' id='favCount'></p>
-					</div>
-				</section>
+		<header className='header'>
+			<div className='content-wrapper header__flex'>
+				<Link to='/' className='header-title'>
+					Movies!
+				</Link>
+				<SearchField />
+				<Favorites />
 			</div>
 		</header>
 	);
