609672699553e520f8f7cb7f0e5536589cc6496c
OwlingPelle <per.gronroos@hotmail.com>
2026-04-14 10:33:53 +0200

    MovieCard klar

---
+++ src/components/MovieCard/MovieCard.jsx
@@ -0,0 +1,26 @@
+import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import './movieCard.css';
+import imdbLogo from '../../resources/imdb-logo.svg';
+
+const MovieCard = ({ movie }) => {
+	return (
+		<Link className='card' to={`/movie/${movie.imdbID}`}>
+			<article className='card-wrapper'>
+				<div className='card__imdb-container'>
+					{/* <img src={imdbLogo} className='card__imdb-icon' alt='IMDB logo' /> */}
+					{/* <p className='card__imdb-rating'>{movie.details.Ratings[0].Value}</p> */}
+					{/* rating måste hämtas från  OMDB api*/}
+				</div>
+				<i className='fa-regular fa-star'></i>
+				<div className='card__image-container'></div>
+				<img src={movie.Poster} className='card__image' alt={movie.Title} data-id={movie.imdbID} />
+				<div className='card__title-container'>
+					<h3 className='card__title'>{movie.Title}</h3>
+				</div>
+			</article>
+		</Link>
+	);
+};
+
+export default MovieCard;
--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,13 +1,29 @@
+import MovieCard from '../../components/MovieCard/MovieCard';
 import { useFetchRecommended } from '../../hooks/useFetchRecommended';
 import { shuffleArray } from '../../utils/shuffleArray';
 import './landingPage.css';
 
 const LandingPage = () => {
-	return <div>LandingPage</div>;
-    const { recommendedMovies, isLoading, isError } = useFetchRecommended();
-    console.log(recommendedMovies);
+	// return <div>LandingPage</div>;
 
-    return <section className="page"></section>;
+	const { recommendedMovies, isLoading, isError } = useFetchRecommended();
+	console.log(recommendedMovies);
+
+	// if (isLoading) return <p>Laddar...</p>;
+	// if (isError) return <p>Nånting gick fel</p>;
+	// if (!recommendedMovies.length) return <p>Inga filmer hittades</p>;
+
+	return (
+		<section className='page-wrapper'>
+			<h2 className='page-title'>Recommended Movies</h2>
+			<section className='page'>
+				{/* <MovieCard movie={recommendedMovies[0]} /> */}
+				{shuffleArray(recommendedMovies).map((movie) => (
+					<MovieCard key={movie.imdbID} movie={movie} />
+				))}
+			</section>
+		</section>
+	);
 };
 
 export default LandingPage;
